2df349f954c21001d56dd88f631a94a9a0c9912a
lutianzhou001 <lutianzhou@yuhu.tech>
2019-05-30 10:02:43 +0800

    add the function of transmit

--- resolvers/mutation/order.js
+++ resolvers/mutation/order.js
@@ -12,56 +12,12 @@ const sendtop = require('../../msg/msghandle/sendmsg/ptmsg')
 
 const order = {
   async transmit(parent, args, ctx, info) {
-    //TODO 需要改动此接口
-    const id = getUserId(ctx)
-    var request = new messages.PostRequest('');
-    request.setOrderid(args.postorder.orderid);    //发布订单的Id(可以通过订单创建接口得到)
-    request.setIsfloat(args.postorder.isfloat);                              //雇佣人数是否浮动
-    request.setHourlysalary(args.postorder.salary);                        //时薪
-    request.setWorkcontent(args.postorder.workcontent);             //工作内容
-    request.setAttention(args.postorder.attention);                   //注意事项
-    client.postOrder(request, function (err, response) {});
-    //TODO  we will add formid into args in graphql
-    // set formid which is created when adviser post order
-    var userId = id
-    var orderId = args.postorder.orderid
-    var formId = args.formid
-    var setRes = await formid.setFormId(userId, orderId, formId)
-    console.log('set formid after creating :', setRes)
-
-    var todo = await handles.AdviserGetOrderList(ctx, id, args.postorder.orderid, 0)
-    var doing = await handles.AdviserGetOrderList(ctx, id, args.postorder.orderid, 1)
-    Array.prototype.push.apply(todo, doing)
-    var hotelUsers = await ctx.prismaHotel.users({ where: { id: todo[0].originorder.hotelid } })
-    // send msg to hotel after posting
-
-    // we will fulfill all the blanks in the messages
-    var advisers = await ctx.prismaHr.users({where :{id:id}})
-    var profiles = await ctx.prismaHr.profiles({where:{user:{id:advisers[0].id}}})
-    var advisername = advisers[0].name
-    var advisercompany = profiles[0].companyname
-    var occupation = todo[0].originorder.occupation
-    if (todo[0].modifiedorder.length){
-      var datetime = todo[0].modifiedorder[0].changeddatetime
-    } else {
-      var datetime = todo[0].originorder.datetime
-    }
-    var date = new Date(datetime*1000)
-    var HotelMsgData = {
-      userId: todo[0].originorder.hotelid,
-      orderId: todo[0].originorder.orderid,
-      openId:  hotelUsers[0].wechat,
-      num: 1,
-      content: {
-        keyword1: advisercompany + ' '+ advisername,
-        keyword2: date.getFullYear()+'年'+ (date.getMonth()+1) +'月'+date.getDate()+'日'+date.getHours()+'时开始' + ' ' + occupation,
-        keyword3: sd.format(new Date(), 'YYYY/MM/DD HH:mm'),
-      }
-    }
-    var sendHRes = await sendtoh.sendTemplateMsgToHotel(HotelMsgData);
-    console.log('send msg to hotel after posting', sendHRes)
-    var error = false
-    return error
+    var request = new messages.TransmitRequest()
+    var agentid = getUserId(ctx)
+    request.setOrderid(args.orderid)
+    request.setAgentid(agentid)
+    client.transmitOrder((request,function(err,response){ console.log("转发成功")}))
+    return true
   }
 }
 
