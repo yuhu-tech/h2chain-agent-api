58f2cd076b27e05d662f6ccbf6fe5226f8a9a314
lutianzhou001 <lutianzhou@yuhu.tech>
2019-05-30 17:05:26 +0800

    fix the bug of cannot use me !

--- resolvers/query/query.js
+++ resolvers/query/query.js
@@ -5,16 +5,17 @@ const utils = require('../../token/ali_token/utils/utils')
 const query = {
   //my information and my profile
   async me(parent, args, ctx, info) {
+    console.log("begin searching me ")
     const id = getUserId(ctx)
     const users = await ctx.prismaAgent.users({ where: { id } })
     const personalmsgs = await ctx.prismaAgent.personalmsgs({ where: { user: { id: id } } })
     var result = {
+      id: id,
       wechat: users[0].wechat,
       personalmsgs: personalmsgs[0]
     }
     return result
   },
-  
   async search(parent, args, ctx, info) {
     const id = getUserId(ctx)
     if (args.state == 12) {
